0fed22cee017802f386ca23b43329b14ac482e2b
root <root@Mirmikas>
2025-06-19 07:27:30 +0200

    Fix the terraform bug in minikube-setup

--- terraform/minikube-setup/outputs.tf
+++ terraform/minikube-setup/outputs.tf
@@ -1,7 +1,7 @@
 output "namespace_names" {
-  value = [for ns in kubernetes_namespace.envs : ns.metadata.name]
+  value = [for ns in kubernetes_namespace.envs : ns.metadata[0].name]
 }
 
 output "mongo_secret_namespaces" {
-  value = [for s in kubernetes_secret_v1.mongo_url : s.metadata.namespace]
+  value = [for s in kubernetes_secret_v1.mongo_url : s.metadata[0].namespace]
 }
