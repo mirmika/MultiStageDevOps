7a8c93aa9e6cd942dee345f4ff1d2569f42ea64c
root <root@Mirmikas>
2025-06-19 07:12:54 +0200

    fix(terraform): correct metadata references in outputs

--- terraform/minikube-setup/outputs.tf
+++ terraform/minikube-setup/outputs.tf
@@ -1,7 +1,7 @@
 output "namespace_names" {
-  value = [for ns in kubernetes_namespace.envs : ns.metadata[0].name]
+  value = [for ns in kubernetes_namespace.envs : ns.metadata.name]
 }
 
 output "mongo_secret_namespaces" {
-  value = [for s in kubernetes_secret_v1.mongo_url : s.metadata[0].namespace]
+  value = [for s in kubernetes_secret_v1.mongo_url : s.metadata.namespace]
 }
